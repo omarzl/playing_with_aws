ea10ee2f0a6d05ff2e1f6d2ea077c655319e7468
Omar Zúñiga Lagunas <omarzl@hotmail.es>
2024-05-18 12:07:44 -0600

    changed call of linux dh infra

--- terraform/linux-dh-infra/main.tf
+++ terraform/linux-dh-infra/main.tf
@@ -1,8 +1,11 @@
-module "linux-infra" {
-  source = "./modules/aws-linux"
 
-  instance_type       = "a1.medium"
-  ami_filter          = "al2023-ami-2023.4.20240513.0-kernel-6.1-x86_64"
-  vpc_id              = var.vpc_id
-  number_of_instances = var.number_of_instances
+module "linux-dh-infra" {
+  source = "../modules/ec2-autoscaling-infra"
+
+  instance_type           = "a1.medium"
+  ami_filter              = "al2023-ami-2023.4.20240513.0-kernel-6.1-x86_64"
+  prefix                  = "linux_dh"
+  vpc_id                  = var.vpc_id
+  max_number_of_instances = var.max_number_of_instances
+  key_name                = var.key_name
 }
