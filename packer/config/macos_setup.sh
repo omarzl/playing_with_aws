cc80d609f4c2ea5606d700b77516623fd3dfdd35
Omar Zúñiga Lagunas <omarzl@hotmail.es>
2024-06-02 19:48:46 -0600

    initial macos setup

--- packer/config/macos_setup.sh
+++ packer/config/macos_setup.sh
@@ -1,1 +1,16 @@
 #!/bin/zsh
+
+# Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+# Bazel
+# brew install bazelisk
+# Xcode
+brew install xcodesorg/made/xcodes
+
+export FASTLANE_SESSION
+xcodes install \
+--latest \
+--experimental-unxip \
+--use-fastlane-auth \
+--no-superuser
