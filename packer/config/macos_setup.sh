c335a8ab8574a99c2ee9e83e0446091839b72636
Omar Zúñiga Lagunas <omarzl@hotmail.es>
2024-06-03 21:31:26 -0600

    updated macos setup

--- packer/config/macos_setup.sh
+++ packer/config/macos_setup.sh
@@ -1,16 +1,27 @@
 #!/bin/zsh
 
-# Homebrew
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-# Bazel
-# brew install bazelisk
-# Xcode
+# Homebrew installation
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+# Bazel installation
+brew install bazelisk
+# Xcodes tool installation
 brew install xcodesorg/made/xcodes
 
+xcode_version=15.4.0
+# Xcode installation
 export FASTLANE_SESSION
 xcodes install \
---latest \
+$xcode_version \
 --experimental-unxip \
 --use-fastlane-auth \
 --no-superuser
+# Selects Xcode
+sudo xcode-select -s /Applications/Xcode-$xcode_version.app
+# Confirms Xcode selection
+xcode-select -p
+# Initial setup
+sudo xcodebuild -license accept
+xcodebuild -runFirstLaunch
+xcodebuild -downloadPlatform iOS
+
