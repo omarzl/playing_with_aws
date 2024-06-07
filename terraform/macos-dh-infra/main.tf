37115f30233a36b50e84a8dbdcf18de4468510a0
Omar Zúñiga Lagunas <omarzl@hotmail.es>
2024-06-07 13:29:41 -0600

    fixed availability zone in mac for terraform

--- terraform/macos-dh-infra/main.tf
+++ terraform/macos-dh-infra/main.tf
@@ -7,7 +7,7 @@ module "macos_dh_infra" {
   prefix                  = "mac_dh"
   vpc_id                  = var.vpc_id
   subnet_id                   = var.subnet_id
-  availability_zones          = ["us-east-1c"] # mac2.metal instances aren't available in us-east-1a
+  availability_zones          = ["us-east-1b"] # mac2.metal instances aren't available in us-east-1a
   associate_public_ip_address = true
   max_number_of_instances = var.max_number_of_instances
   key_name                = var.key_name
